19534e085067f3332ac6ff1c6fd49e0c702b19ac
mary-lev <marylevchenko@gmail.com>
2020-11-30 21:17:53 +0300

    add comments to views, rename index

--- isite/views.py
+++ isite/views.py
@@ -7,13 +7,21 @@ from .forms import PostForm
 
 
 class ViewMixin(View):
+    """
+    Основа для страниц сайта:
+    передаются последние записи и страницы для навигации.
+    """
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
         context['pages'] = Page.objects.filter(parent__isnull=True)
         context['fresh_posts'] = Post.objects.all()[:5]
         return context
 
-class index(ViewMixin, ListView):
+
+class IndexView(ViewMixin, ListView):
+    """
+    Главная страница сайта: вывод пяти последних постов.
+    """
     model = Post
     paginate_by = 5
     template_name = 'index.html'
@@ -21,14 +29,23 @@ class index(ViewMixin, ListView):
 
 
 class PostDetailView(ViewMixin, DetailView):
+    """
+    Шаблон для одного поста.
+    """
     model = Post
 
 
 class PageDetailView(ViewMixin, DetailView):
+    """
+    Шаблон для одной страницы.
+    """
     model = Page
 
 
 class PostCreateView(CreateView):
+    """
+    Форма для создания нового поста.
+    """
     model = Post
     form_class = PostForm
     template_name = 'post_new.html'
