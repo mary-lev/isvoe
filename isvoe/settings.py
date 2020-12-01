817573486fc0dbebe6072d6b7e5ee555f2d1e1a3
mary-lev <marylevchenko@gmail.com>
2020-12-01 20:45:53 +0300

    hide secret

--- isvoe/settings.py
+++ isvoe/settings.py
@@ -1,15 +1,11 @@
 from pathlib import Path
 import os
 
+from .secret import SECRET_KEY
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$2u%nj@m8hk32%qt+j+xk&#$b6^3q(++6!g%uy(m@j)x7cz#de'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
